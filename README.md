31314757e9b9717c0f4c704fabe991eb13917376
洮羱芝闇 <shun_@outlook.com>
2022-04-24 18:50:44 +0800

    update

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # MyPrivateServer1
 ### 我的私有服务器1
-
-#### 【2022.4.24】版本号3
-- 访问地址[![www.tyza66.com](https://img.shields.io/badge/www.tyza66.com-grey)](http://www.tyza66.com/)  
-- 更新了欢迎界面
-- 只能在白天访问，因为晚上寝室会断电  
-
-#### 【2022.4.24】版本号4
-- 关闭测试模式，启功正式模式  
-
 By:tyza66(洮羱芝闇)
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, jsonify, render_template, sessions, send_file
+from flask import request
+from flask_cors import CORS
+from gevent import pywsgi
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html');
+    return render_template('index.html')
 
+def runit():
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+    print("启动服务器")
+    server.serve_forever()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    runit()
