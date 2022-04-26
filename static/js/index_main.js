0d3e334387bd5babc570b7af9cf59f17890d2322
洮羱芝闇 <shun_@outlook.com>
2022-04-26 12:14:42 +0800

    update

--- static/js/index_main.js
+++ static/js/index_main.js
@@ -1,6 +1,13 @@
 //获取控件
-github = document.querySelector("#github");
+    //按钮
+    github = document.querySelector("#github");
+    info_close = document.querySelector("#info_close");
+    //框
+    info = document.querySelector("#info");
 //添加监听器
 github.addEventListener("click", () => {
     window.location.href = "https://github.com/tyza66/";
+});
+info_close.addEventListener("click", () => {
+    info.hidden = true;
 });
