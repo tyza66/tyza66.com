ed58f35aa65f92bd1c0674c4a4cf430e35dee928
洮羱芝闇 <shun_@outlook.com>
2022-04-26 22:10:21 +0800

    update

--- README.md
+++ README.md
@@ -13,4 +13,13 @@
 - 备线[![tyza66.free.idcfengye.com/](https://img.shields.io/badge/tyza66.free.idcfengye.com/-grey)](http://tyza66.free.idcfengye.com/) 
 - 备线[![3a697k2501.zicp.vip/](https://img.shields.io/badge/3a697k2501.zicp.vip/-grey)](http://3a697k2501.zicp.vip/) 
 
+#### 【2022.4.26】版本号5
+- 更新了欢迎界面：添加了一条声明网站仅用来接口测试的消息
+- 添加了favicon.ico图标
+
+#### 【2022.4.26】版本号6
+- 更新验证连通性接口'/online'
+- 更新获得时间戳以意图计算延迟接口'/ping'
+- 更新了欢迎界面：增加了一颗网易云音乐播放小标签
+
 By:tyza66(洮羱芝闇)
--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask, jsonify, render_template, sessions, send_file
 from flask import request
 from flask_cors import CORS
 from gevent import pywsgi
+import time
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/online')
+def online():
+    return '1';
+
+@app.route('/ping')
+def ping():
+    return str(time.time())
 
 def runit():
     server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
